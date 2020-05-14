90bf7616dc203abbf1f7a6e8dd5512018f885fac
hackerrank <hackerrank.com>
2020-05-14 17:18:51 -0400

    A* fix

--- src/Resources/AStar.java
+++ src/Resources/AStar.java
@@ -14,55 +14,59 @@ import java.util.List;
 import java.util.PriorityQueue;
 
 public class AStar {
-	
-	
-	public LinkedList<Node> aStarSearch(Graph graph, Node start) {
 
-        //Creating the Node list and the Path list 
-        LinkedList<Node> nodeList = new LinkedList<Node>(); // Nodes list
-        LinkedList<Node> visitedList = new LinkedList<Node>(); // Path list / Visited list
-        
-        
-        start.heuristic=0;
-        nodeList.add(start);
-        visitedList.add(start);
-        
-        while(nodeList.size()!=0) { // Loop to run through the nodes available
-        	
-        	int smallestDistance = nodeList.get(0).heuristic;
-        	int g=0;
-        	
-        	for(int i = 0; i < nodeList.size()-1; i++) { // Loop to find the node with the lowest distance
-        		if(nodeList.get(i).isGoal) {
-        			visitedList.add(visitedList.get(i));
-        			return nodeList; // Found and finish : fix to add the path and stuff
-        		}
-        		if(nodeList.get(i).heuristic < smallestDistance) {
-        			smallestDistance = nodeList.get(i).heuristic;
-        			g = i; // Save the index to access later
-        		}
-        		
-        	}
-        	
+	private class RouteNode{
+		RouteNode previousNode;
+		Node currentNode;
+		float pathCost;
 
-        	LinkedList<Edge> edgeList = graph.getEdges(nodeList.remove(g)); //Create a linked list of the nodes
-        																	//connected to the node I'm looking at
-        	float shortestEdge = edgeList.get(0).getCost();
-        	int d = 0;
-        	
-        	for(int i = 0; i < edgeList.size(); i++) { //Looks through the edges of the node to find the shortest
-        		nodeList.add(edgeList.get(i).destination); //Add all destinations to the list to look at later
-        		if(edgeList.get(i).getCost() < shortestEdge) {
-        			shortestEdge = edgeList.get(i).distance;
-        			d = i;
-        		}
-        	}
-        	nodeList.add(edgeList.get(d).destination); //Adds the shortest node to the path list "nodeList"
-   	
-        }
-        
-        return nodeList;
+		public RouteNode(Node currentNode, RouteNode previousNode, float pathCost) {
+			this.currentNode = currentNode;
+			this.previousNode = previousNode;
+			this.pathCost = pathCost;
+		}
 	}
-	
 
+
+	public RouteNode aStarSearch(Graph graph, Node start) {
+
+		//Creating the Node list and the Path list 
+		LinkedList<RouteNode> nodeList = new LinkedList<RouteNode>(); // Nodes list
+		LinkedList<Node> visitedList = new LinkedList<Node>(); // Path list / Visited list
+
+
+		nodeList.add(new RouteNode(start, null, 0));
+		visitedList.add(start);
+
+		while(nodeList.size()!=0) { // Loop to run through the nodes available
+
+			float minF = nodeList.get(0).pathCost + nodeList.get(0).currentNode.heuristic/45;
+			RouteNode lowestNode = nodeList.get(0);
+
+			for(RouteNode n : nodeList) { // Loop to find the node with the lowest distance
+				if(n.currentNode.isGoal) {
+					return n; // Found and finish : fix to add the path and stuff
+				}
+				if(n.pathCost + n.currentNode.heuristic < minF) {
+					minF = n.pathCost + n.currentNode.heuristic;
+					lowestNode = n;
+				}
+
+			}
+
+
+			LinkedList<Edge> edgeList = graph.getEdges(lowestNode.currentNode); //Create a linked list of the nodes
+			//connected to the node I'm looking at
+
+			for(Edge e : edgeList) { //Looks through the edges of the node to find the shortest
+				if(!e.destination.equals(lowestNode.previousNode.currentNode)) {
+
+					float pathCost = e.distance/(e.speedLimit*e.traffic) + lowestNode.pathCost;
+					nodeList.add(new RouteNode(e.destination, lowestNode, pathCost)); //Add all destinations to the list to look at later
+
+				}
+			}
+		}
+		return null;
+	}
 }
